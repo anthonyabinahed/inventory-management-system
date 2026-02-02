9e8a0f5ccb445def9d2a1cccd435b3df6e51d40d
anthonyabinahed <anthonyabinahed@gmail.com>
2026-02-02 09:56:33 +0100

    removed unecessary code

--- app/layout.js
+++ app/layout.js
@@ -1,5 +1,4 @@
 import { Inter } from "next/font/google";
-import { getSEOTags } from "@/libs/seo";
 import ClientLayout from "@/components/LayoutClient";
 import config from "@/config";
 import "./globals.css";
@@ -12,10 +11,6 @@ export const viewport = {
 	width: "device-width",
 	initialScale: 1,
 };
-
-// This adds default SEO tags to all pages in our app.
-// You can override them in each page passing params to getSOTags() function.
-export const metadata = getSEOTags();
 
 export default function RootLayout({ children }) {
 	return (
